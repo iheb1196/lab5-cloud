39b690a1bf47b3ea3e6f410692c601090ee7d446
iheb.jendoubi@medtech.tn <iheb.jendoubi@medtech.tn>
2021-12-06 14:48:32 +0100

    add

---
+++ data/seeds/001_first-seed.js
@@ -0,0 +1,23 @@
+const profiles = [
+  {
+  name: 'Imen'
+  },
+  {
+  name: 'Aymen'
+  },
+  {
+  name: 'Feres'
+  },
+  {
+  name: 'Nadia'
+  },
+  {
+  name: 'Mohamed'
+  }
+ ];
+ exports.seed = function (knex) {
+  return knex('profiles').del()
+  .then(() => {
+  return knex('profiles').insert(profiles)
+  })
+ };
